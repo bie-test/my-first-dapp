870f661e9f40b808df5d1231e7840c45b4232706
Sirinthiya Chaitani <sirinthiya_c@BKC.CO.TH>
2018-11-07 10:21:41 +0700

    คำสั่ง deploy

--- contracts/ChainList.sol
+++ contracts/ChainList.sol
@@ -2,24 +2,29 @@
 pragma solidity ^0.4.18;
 
 contract ChainList {
+    
     address seller;
     string name;
     string description;
-    unit256 price;
-    
+    uint256 price;
+
+	// Sell thing info
     function sellArticle(string _name, string _description, uint256 _price) public {
         seller = msg.sender;
         name = _name;
         description = _description;
         price = _price;
+    }
 
-    }
-    fuction getArticle() public view return {
+	// get thing info
+    function getArticle() public view returns (
         address _seller,
-        string  _name,
+        string _name,
         string _description,
-        unin256 _price
-    }{
-        return(seller,name,description,price);
+        uint256 _price
+    ) {
+
+        return (seller, name, description, price);
     }
+
 }
