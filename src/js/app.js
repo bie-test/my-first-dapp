44e266fc7cd56c911b62e06cbcf659ee29fb9f27
Sirinthiya Chaitani <sirinthiya_c@BKC.CO.TH>
2018-11-07 11:31:07 +0700

    แสดงรายการ ขาย

--- src/js/app.js
+++ src/js/app.js
@@ -3,12 +3,21 @@ App = {
      contracts: {},
 
      init: function() {
-          /*
-           * Replace me...
-           */
+      // load articleRow
+      var articlesRow = $('#articlesRow');
+      var articleTemplate = $('#articleTemplate');
 
-          return App.initWeb3();
-     },
+      articleTemplate.find('.panel-title').text('สินค้าชิ้นที่่ 1');
+      articleTemplate.find('.article-description').text('รายละเอียดสินค้า ชิ้นที่ 1');
+      articleTemplate.find('.article-price').text('10.24');
+      articleTemplate.find('.article-seller').text('ชื่อผู้ขาย');
+
+      articlesRow.append(articleTemplate.html());
+
+      return App.initWeb3();
+ },
+
+
 
      initWeb3: function() {
           /*
