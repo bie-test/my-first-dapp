6266494c9efc0df22cc85fa71c920ed7fd1bb3d1
Sirinthiya Chaitani <sirinthiya_c@BKC.CO.TH>
2018-11-07 11:42:36 +0700

    create web3function

--- src/js/app.js
+++ src/js/app.js
@@ -4,15 +4,15 @@ App = {
 
      init: function() {
       // load articleRow
-      var articlesRow = $('#articlesRow');
-      var articleTemplate = $('#articleTemplate');
+      // var articlesRow = $('#articlesRow');
+      // var articleTemplate = $('#articleTemplate');
 
-      articleTemplate.find('.panel-title').text('สินค้าชิ้นที่่ 1');
-      articleTemplate.find('.article-description').text('รายละเอียดสินค้า ชิ้นที่ 1');
-      articleTemplate.find('.article-price').text('10.24');
-      articleTemplate.find('.article-seller').text('ชื่อผู้ขาย');
+      // articleTemplate.find('.panel-title').text('สินค้าชิ้นที่่ 1');
+      // articleTemplate.find('.article-description').text('รายละเอียดสินค้า ชิ้นที่ 1');
+      // articleTemplate.find('.article-price').text('10.24');
+      // articleTemplate.find('.article-seller').text('ชื่อผู้ขาย');
 
-      articlesRow.append(articleTemplate.html());
+      // articlesRow.append(articleTemplate.html());
 
       return App.initWeb3();
  },
@@ -20,13 +20,17 @@ App = {
 
 
      initWeb3: function() {
-          /*
-           * Replace me...
-           */
+           
+      App.web3Provider = new web3.provider.httpProvider('http://localhost:7545');
+      web3 = new web3(App.web3Provider);
+      App.displayaccountinfo();
+      
 
           return App.initContract();
      },
-
+displayaccountinfo: function() {
+      
+}
      initContract: function() {
           /*
            * Replace me...
